880f229ee03f41d3604d6d9bb8ae984823658ad1
JakubPala <pala.jakub@seznam.cz>
2024-12-01 19:38:35 +0100

    první-nástřel

---
+++ Js/script.js
@@ -0,0 +1,68 @@
+(function($){
+    $(function(){
+        /* Scroll to section */
+        $(".jq--scroll-my-service").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--my-service").offset().top}, 1000);
+        });
+
+        $(".jq--scroll-about-me").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--about-me").offset().top}, 1500);
+        });
+
+        $(".jq--scroll-references").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--references").offset().top}, 1500);
+        });
+
+        $(".jq--scroll-my-projekts").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--my-projekts").offset().top}, 1500);
+        });
+
+        $(".jq--scroll-contact-form").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--contact-form").offset().top}, 1500);
+        });
+
+        /* Scroll buttons to section */
+        $(".jq--scroll-button-order-services").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--contact-form").offset().top}, 1500);
+        });
+
+        $(".jq--scroll-button-references").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--references").offset().top}, 1500);
+        });
+
+        /* Mobile navigation */
+        $(".jq--nav-icon").click(function(){
+
+            $(".nav-background").fadeToggle();
+            $(".mobile-nav-back").fadeToggle();
+            $("nav ul").fadeToggle();
+        });
+
+        /* Change hamburger to cross and other way */
+        $(".jq--image-hamburger").click(function(){
+
+
+            if($(".jq--image-hamburger").attr("src") == "Obrazky/menu button.png")   
+            {
+                $($(".jq--image-hamburger").attr("src","Obrazky/close button.png"));
+            }
+            else
+            {
+                $($(".jq--image-hamburger").attr("src","Obrazky/menu button.png"));
+            }
+        
+        });
+
+        /* nav-background-scrolled */
+        const headerEl = document.querySelector(".nav-background");
+
+        window.addEventListener("scroll", () => {
+          if (window.scrollY > 50) {
+            headerEl.classList.add("nav-background-scrolled");
+        } else if (window.scrollY <= 50) {
+            headerEl.classList.remove("nav-background-scrolled");
+        }
+        });
+                    
+    });
+})(jQuery);
